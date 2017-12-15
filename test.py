bb4969ea8cf712f1328d38e56d5d9d125425ad38
rasc33 <skamijo@linux.com>
2017-12-14 20:34:17 -0800

    fix some line

--- test.py
+++ test.py
@@ -6,25 +6,26 @@ from bittrex.bittrex import API_V2_0, Bittrex
 apiKeyFile = "../apikey.txt"
 apiSecretFile = "../apisecret.txt"
 
-f = open(apiKeyFile)
+f = open(apiKeyFile, encoding='ASCII')
 apikey = f.read()
 f.close()
 print (apikey)
 
-f = open(apiSecretFile)
+f = open(apiSecretFile, encoding='ASCII')
 apisecret = f.read()
 f.close()
 print (apisecret)
 
 
 my_Bittrex = Bittrex(apikey, apisecret, api_version=API_V2_0)
-print (my_Bittrex.get_balances())
+#print (my_Bittrex.get_balances())
 
-my_Bittrex.get_balance('BTC')
+
+btc_balance = my_Bittrex.get_balance('BTC')
 {'success': True, 
  'message': '',
  'result': {'Currency': 'BTC', 'Balance': 0.0, 'Available': 0.0, 
             'Pending': 0.0, 'CryptoAddress': None}
 }
 
-
+print (btc_balance)
