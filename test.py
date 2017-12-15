27df74f885b495f2ad5a4be31e9f90e9ec38b485
rasc33 <skamijo@linux.com>
2017-12-14 20:26:38 -0800

    test script

--- test.py
+++ test.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python
+# coding: UTF-8
+
+from bittrex.bittrex import API_V2_0, Bittrex
 
 apiKeyFile = "../apikey.txt"
+apiSecretFile = "../apisecret.txt"
+
+f = open(apiKeyFile)
+apikey = f.read()
+f.close()
+print (apikey)
+
+f = open(apiSecretFile)
+apisecret = f.read()
+f.close()
+print (apisecret)
+
+
+my_Bittrex = Bittrex(apikey, apisecret, api_version=API_V2_0)
+print (my_Bittrex.get_balances())
+
+my_Bittrex.get_balance('BTC')
+{'success': True, 
+ 'message': '',
+ 'result': {'Currency': 'BTC', 'Balance': 0.0, 'Available': 0.0, 
+            'Pending': 0.0, 'CryptoAddress': None}
+}
+
 
